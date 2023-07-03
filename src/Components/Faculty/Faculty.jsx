646dfe27535d6c941fa4b07e8a554a158ad0e748
Shakir <shakirali.sacube@gmail.com>
2023-07-03 17:28:07 +0530

    Added pagination. Data in the home page now gets fetched from the database
    setup for searching in pages
    
    Hotfix: Fix infinite loop caused by useEffect

--- src/Components/Faculty/Faculty.jsx
+++ src/Components/Faculty/Faculty.jsx
@@ -1,46 +1,27 @@
 import React from 'react';
-import PageControls from '../PageControls/PageControls';
-import { BsFillPersonPlusFill } from "react-icons/bs"
-import TableCard from '../Common/TableCard';
-import PageHeader from "../PageHeader/PageHeader"
-<<<<<<< HEAD
-=======
+import PageControls from '../Common/PageControls';
 import { BsFillPersonFill } from "react-icons/bs"
+import TableCard from '../Common/TableCard';
+import PageHeader from "../Common/PageHeader"
 import { readDocuments } from '../../Controllers/index';
 import './Faculty.css'
->>>>>>> 22fcfa7 (table cards 2)
 
 const FacultyPage = () => {
+
+  const searchParam = new URLSearchParams(document.location.search)
+  const searchData = searchParam.get('search')
 
   const facultyData = readDocuments("faculty");
   console.log({facultyData});
   return (
     <>
-<<<<<<< HEAD
-      <PageHeader icon={<BsFillPersonPlusFill/>} title="Faculty"></PageHeader>
-      <PageControls inputplaceholder='Search Faculty'/>
-
-      <ul style={{listStyleType: 'none'}}>
-          <li>
-          <FacultyCard row1='Abhijaan Ganguly' row2='21bcac05@kristujayanti.com' row3='Computer Science(UG)' row4='UBA' />
-          </li>
-          <li>
-          <FacultyCard row1='Abhijaan Ganguly' row2='21bcac05@kristujayanti.com'  row4='UBA' />
-          </li>
-          <li>
-          <FacultyCard row1='Abhijaan Ganguly'  row3='Computer Science(UG)' row4='UBA' />
-          </li>
-          <li>
-          <FacultyCard row1='Abhijaan Ganguly' />
-          </li>
-        </ul>
-    </>
-
-  )
-=======
     <PageHeader title="Faculty" icon={<BsFillPersonFill />} />
+    <PageControls
+      inputplaceholder="Search Faculty"
+      setSearchText = {searchData}
+      pageSlug={"faculty"}
+    />
     <div className='main-container'>
-      <PageControls inputplaceholder="Search Faculty" />
       <div className='table-container'>
         <ul style={{listStyleType: 'none'}} >
           {/* {facultyData.map((faculty) => 
@@ -93,7 +74,6 @@ const FacultyPage = () => {
     </div>
   </>
   );
->>>>>>> 22fcfa7 (table cards 2)
 }
 
 export default FacultyPage
